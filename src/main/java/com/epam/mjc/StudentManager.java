e52a8974593be0236b7c01749704ec2eed783d04
Renata Yermak <renatkahturo@gmail.com>
2023-04-10 18:13:55 +0300

    .

--- src/main/java/com/epam/mjc/StudentManager.java
+++ src/main/java/com/epam/mjc/StudentManager.java
@@ -1,21 +1,25 @@
 package com.epam.mjc;
 
+import com.epam.mjc.exception.StudentNotFoundException;
 
 public class StudentManager {
 
-  private static final long[] IDs = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11};
+    private static final long[] IDs = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11};
 
-  public Student find(long studentID) {
-    return Student.getValueOf(studentID);
-  }
-
-  public static void main(String[] args) {
-    StudentManager manager = new StudentManager();
-
-    for (int i = 0; i < IDs.length; i++) {
-      Student student = manager.find(IDs[i]);
-      System.out.println("Student name " + student.getName());
+    public Student find(long studentID) {
+        for (long id : IDs) {
+            if (id == studentID)
+                return Student.getValueOf(studentID);
+        }
+        throw new StudentNotFoundException("Could not find student with ID " + studentID);
     }
 
-  }
+    public static void main(String[] args) {
+        StudentManager manager = new StudentManager();
+
+        for (int i = 0; i < IDs.length; i++) {
+            Student student = manager.find(IDs[i]);
+            System.out.println("Student name " + student.getName());
+        }
+    }
 }
---
+++ src/main/java/com/epam/mjc/exception/StudentNotFoundException.java
@@ -0,0 +1,8 @@
+package com.epam.mjc.exception;
+
+public class StudentNotFoundException extends IllegalArgumentException {
+
+    public StudentNotFoundException(String message) {
+        super(message);
+    }
+}
